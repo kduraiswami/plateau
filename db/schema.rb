57b3625c3c7c2c7ac08099e6aba965b7cf155657
Kailash <kaidurai@gmail.com>
2015-08-29 15:42:02 -0700

    added downcasing of email before save on user objects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829223141) do
+ActiveRecord::Schema.define(version: 20150829224008) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
