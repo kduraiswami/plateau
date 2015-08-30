157cc0f2b2bd110aa0b4043d508d216745b72cc0
Kailash <kaidurai@gmail.com>
2015-08-30 11:09:27 -0700

    added user resources to route file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  resources :users
+
+end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -61,4 +64,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
