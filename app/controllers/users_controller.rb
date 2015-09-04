563e6075733cb4f64a269813395e18a4e0a4b56f
Kailash <kaidurai@gmail.com>
2015-09-03 17:31:33 -0700

    quick change to flash message to enable better user experience on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Welcome to the Sample App!"
+			flash[:success] = "Thank you for signing up, please share your daily workout and learn from others"
 			redirect_to @user
 		else
 			render 'new'
