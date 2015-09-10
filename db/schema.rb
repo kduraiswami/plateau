f43a8eb8ba8f28f2df4e4d208ccfbd1401809fac
Kailash <kaidurai@gmail.com>
2015-09-09 17:08:43 -0700

    initial migration of workout model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909235429) do
+ActiveRecord::Schema.define(version: 20150910000807) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -28,5 +28,16 @@ ActiveRecord::Schema.define(version: 20150909235429) do
     t.string   "biography"
     t.string   "current_practice"
   end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string   "fitness_goal"
+    t.time     "estimated_time"
+    t.string   "gym_style"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "workouts", ["user_id"], name: "index_workouts_on_user_id"
 
 end
