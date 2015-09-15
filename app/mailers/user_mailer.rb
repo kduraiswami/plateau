1b4f5eb22d0fe49a7d60cc587a2dca78793b8684
Kailash <kaidurai@gmail.com>
2015-09-14 18:10:29 -0700

    added simple command for email to send from mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,8 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
+	def password_reset
+		@user = user
+		mail to: user.email, subject: "Password reset"
+	end
+	
 end
