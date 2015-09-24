8a0545a987b9abc20e60a1b9f90aead1d6ebbc18
Kailash <kaidurai@gmail.com>
2015-09-23 21:47:37 -0700

    dropped database and redid migration because models still had activated field even though I don't want that anymore

--- db/schema.rb
+++ db/schema.rb
@@ -25,18 +25,6 @@ ActiveRecord::Schema.define(version: 20150917005051) do
 
   add_index "exercises", ["workout_id"], name: "index_exercises_on_workout_id"
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "likes"
-    t.integer  "dislikes"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
   create_table "tags", force: :cascade do |t|
     t.string   "tag"
     t.datetime "created_at", null: false
@@ -46,14 +34,11 @@ ActiveRecord::Schema.define(version: 20150917005051) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",             default: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
+    t.boolean  "admin",            default: false
     t.string   "location"
     t.string   "biography"
     t.string   "current_practice"
